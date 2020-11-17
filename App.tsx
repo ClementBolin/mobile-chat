c4e35947f263be8ecf57e61986aceebb6600d721
Clement Bolin <clement.bolin@epitech.eu>
2020-11-17 17:03:47 +0100

    add(master): routing

--- App.tsx
+++ App.tsx
@@ -1,13 +1,62 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello in Home</Text>
+    </View>
+  )
+}
+
+const Home2 = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello in Home 2</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconeName
+              
+              switch (route.name) {
+                case "Home":
+                  iconeName = focused
+                    ? 'ios-information-circle'
+                    : 'ios-information-circle-outline';
+                    break
+                case "Home2":
+                  iconeName = focused ?  'ios-list-box' : 'ios-list'
+                  break
+                default:
+                  iconeName = ""
+                  break
+              }
+              return <Ionicons name={iconeName} size={size} color={color} />
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray'
+          }}
+        >
+          <Tab.Screen name="Home" component={Home} options={{ tabBarBadge: 3 }} />
+          <Tab.Screen name="Home2" component={Home2} />
+        </Tab.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
   );
 }
 
@@ -17,5 +66,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
