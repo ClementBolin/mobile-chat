997951cccf570f281cfabf5a0a151a0415867e9c
Clement Bolin <clement.bolin@epitech.eu>
2020-11-24 16:56:16 +0100

    add ChatScreen

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingViewBase } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat';
+import { SafeAreaView } from 'react-navigation';
+import Firebase from '../utils/firebase';
 
-export const ChatScreen = () => {
+export const ChatScreen = ({ navigation }) => {
+    const [messages, setMessages] = useState([]);
+    const [actualUser, setActualUser] = useState({
+        _id: "",
+        name: ""
+    })
+
+    const user = () => {
+        // return {
+        //     _id: Firebase.uid,
+        //     name: navigation.state.params.name
+        // }
+        // setActualUser(Firebase.uid, navigation.state.params.name)
+    }
+
+    useEffect(() => {
+        Firebase.get((message: any) => setMessages(GiftedChat.append(messages, message)))
+    }, [])
+
+    const chat = <GiftedChat messages={messages} onSend={Firebase.send} user={actualUser} />
+    if (Platform.OS === "android") {
+        return (
+            <KeyboardAvoidingViewBase style={{flex: 1}} behavior="padding" keyboardVerticalOffset={30} enabled >
+                {chat}
+            </KeyboardAvoidingViewBase>
+        )
+    }
     return (
-        <View style={styles.container}>
-            <Text>Chat Screen</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            {chat}
+        </SafeAreaView>
     )
 }
 
